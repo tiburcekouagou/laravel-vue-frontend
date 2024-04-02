3de51ef5307a05fe296a07e050204e455dd1840d
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-04-02 21:42:47 +0100

    Implemented the user fonctionality

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,29 +1,61 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axiosClient from '@/axios'
+import { ref } from 'vue'
+
+interface RegisterPayload {
+  name: string
+  email: string
+  password: string
+  password_confirmation: string
+}
+
+const form = ref<RegisterPayload>({
+  name: '',
+  email: '',
+  password: '',
+  password_confirmation: ''
+})
+
+function register(payload: RegisterPayload) {
+  axiosClient
+    .get('/sanctum/csrf-cookie', {
+      baseURL: 'http://localhost:8000'
+    })
+    .then(async (response) => {
+      const res = await axiosClient.post('/register', payload)
+      console.log(res)
+    })
+}
+</script>
 <template>
   <div class="register">
     <h1>Register</h1>
-    <form>
+    <form @submit.prevent="register(form)">
       <label>
         <div>Name</div>
-        <input type="text" />
+        <input type="text" v-model="form.name" />
       </label>
 
       <label>
         <div>Email</div>
-        <input type="email" />
+        <input type="email" v-model="form.email" />
       </label>
 
       <label>
         <div>Password</div>
-        <input type="password" />
+        <input type="password" v-model="form.password" />
       </label>
 
       <label>
         <div>Confirm Password</div>
-        <input type="confirm_password" />
+        <input type="password" v-model="form.password_confirmation" />
       </label>
 
-      <button class="btn">Register</button>
+      <button
+        class="text-white bg-gray-700 hover:bg-gray-800 inline-block mt-3 px-3 py-2 rounded-sm shadow-sm"
+      >
+        Register
+      </button>
     </form>
 
     <p>
@@ -32,3 +64,8 @@
     </p>
   </div>
 </template>
+<style scoped>
+input {
+  @apply border border-gray-400 bg-slate-100 rounded-md block w-full;
+}
+</style>
