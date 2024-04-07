b1ecaab07d902f215987a70c4578ad22ac7b122a
Tiburce Kouagou <kouagoutiburce@gmail.com>
2024-04-07 22:25:53 +0100

    redirect to login page when user is logged out

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,5 @@
+import { getAxiosInstance } from '@/axios'
+import { AxiosError } from 'axios'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -10,12 +12,14 @@ const router = createRouter({
         {
           path: '/login',
           name: 'login',
-          component: () => import('@/views/LoginView.vue')
+          component: () => import('@/views/LoginView.vue'),
+          meta: { requiresAuth: false }
         },
         {
           path: '/register',
           name: 'register',
-          component: () => import('@/views/RegisterView.vue')
+          component: () => import('@/views/RegisterView.vue'),
+          meta: { requiresAuth: false }
         }
       ]
     },
@@ -26,36 +30,67 @@ const router = createRouter({
         {
           path: '',
           name: 'home',
-          component: () => import('@/views/HomeView.vue')
+          component: () => import('@/views/HomeView.vue'),
+          meta: { requiresAuth: true }
         },
         {
           path: '/me',
           name: 'me',
-          component: () => import('@/views/MeView.vue')
+          component: () => import('@/views/MeView.vue'),
+          meta: { requiresAuth: true }
         },
         {
           path: '/links',
           name: 'links',
-          component: () => import('@/views/links/IndexView.vue')
+          component: () => import('@/views/links/IndexView.vue'),
+          meta: { requiresAuth: true }
         },
         {
           path: '/links/create',
           name: 'links.create',
-          component: () => import('@/views/links/CreateView.vue')
+          component: () => import('@/views/links/CreateView.vue'),
+          meta: { requiresAuth: true }
         },
         {
           path: '/links/:id',
           name: 'links.show',
-          component: () => import('@/views/links/ShowView.vue')
+          component: () => import('@/views/links/ShowView.vue'),
+          meta: { requiresAuth: true }
         },
         {
           path: '/logout',
           name: 'logout',
-          component: () => import('@/views/LogoutView.vue')
+          component: () => import('@/views/LogoutView.vue'),
+          meta: { requiresAuth: true }
         }
       ]
     }
   ]
+})
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    // must be declared by every route
+    requiresAuth: boolean
+  }
+}
+
+router.beforeEach(async (to, from, next) => {
+  if (to.meta.requiresAuth) {
+    let canLogin = false
+    try {
+      const axiosClient = await getAxiosInstance()
+      const resp = await axiosClient.get('/user')
+      canLogin = true
+    } catch (error) {
+      if (error instanceof AxiosError && error.response?.status === 401) {
+        canLogin = false
+      }
+    } finally {
+      if (canLogin) next()
+      else next({ name: 'login' })
+    }
+  } else next()
 })
 
 export default router
