13058e9d3785fbca733a673d3e6a2a0d32961675
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-04-24 15:40:25 +0200

    manually handle form validation errors

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,32 +1,53 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useAuth } from '@/composables/useAuth'
-import { useRouter } from 'vue-router';
+import { AxiosError } from 'axios';
+
+interface LoginErrors {
+  [key: string]: string[];
+}
 
 const form = ref({
   email: '',
   password: ''
 })
-const router = useRouter()
-const { login } = useAuth(router)
+
+const errors = ref<LoginErrors>({
+  email: [],
+  password: [],
+});
+
+const { login } = useAuth()
+async function handleLogin() {
+  try {
+    await login(form.value);
+  } catch (err: any) {
+    if (err instanceof AxiosError && err.response?.status === 422) {
+      errors.value = err.response.data.errors;
+    }
+  }
+}
+
 </script>
 
 <template>
   <div class="login">
     <h1>Login</h1>
-    <form @submit.prevent="login(form)" class="space-y-2">
+    <form @submit.prevent="handleLogin" class="space-y-2">
       <label>
         <div>Email</div>
         <input class="p-2 outline-none border block w-full" type="text" v-model="form.email" />
+        <div class="text-red-600 p-1" v-for="error in errors.email" :key="error">{{ error  }}</div>
       </label>
-
+      
       <label>
         <div>Password</div>
         <input
-          class="p-2 outline-none border block w-full"
-          type="password"
-          v-model="form.password"
+        class="p-2 outline-none border block w-full"
+        type="password"
+        v-model="form.password"
         />
+        <div class="text-red-600 p-1" v-for="error in errors.password" :key="error">{{ error  }}</div>
       </label>
       <button class="px-2 py-1 text-white bg-gray-700 rounded-md">Login</button>
     </form>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 import { useAuth } from '@/composables/useAuth'
+import { AxiosError } from 'axios';
 import { ref } from 'vue'
 import { useRouter } from 'vue-router';
+
+interface RegisterErrors {
+  [key: string]: string[];
+}
+
+const errors = ref<RegisterErrors>({
+  name: [],
+  email: [],
+  password: [],
+})
 
 const form = ref({
   name: '',
@@ -9,27 +20,39 @@ const form = ref({
   password: '',
   password_confirmation: ''
 })
-const router = useRouter()
 
-const { register } = useAuth(router)
+const { register } = useAuth()
+
+async function handleRegistration() {
+  try {
+  await register(form.value);
+  } catch(err: any) {
+    if (err instanceof AxiosError && err.response?.status === 422) {
+      errors.value = err.response.data.errors
+    }
+  }
+}
 </script>
 <template>
   <div class="register">
     <h1>Register</h1>
-    <form @submit.prevent="register(form)">
+    <form @submit.prevent="handleRegistration">
       <label>
         <div>Name</div>
         <input type="text" v-model="form.name" />
+        <div class="text-red-600 p-1" v-for="error in errors.name" :key="error">{{ error }}</div>
       </label>
 
       <label>
         <div>Email</div>
         <input type="email" v-model="form.email" />
+        <div class="text-red-600 p-1" v-for="error in errors.email" :key="error">{{ error  }}</div>
       </label>
 
       <label>
         <div>Password</div>
         <input type="password" v-model="form.password" />
+        <div class="text-red-600 p-1" v-for="error in errors.password" :key="error">{{ error  }}</div>
       </label>
 
       <label>
