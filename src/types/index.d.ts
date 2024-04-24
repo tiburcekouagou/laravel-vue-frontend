4f5ea5078bb9a41e799d78d969cc4bcf2d4c2988
Kouagou Tiburce <kouagoutiburce@gmail.com>
2024-04-24 16:10:40 +0200

    added some application-wide ts interfaces

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -20,3 +20,36 @@ export interface RegisterPayload {
   password: string
   password_confirmation: string
 }
+
+export interface Link {
+  short_link: string;
+  full_link: string;
+  views: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface ErrorResponse {
+  message: string;
+  errors: Record<string, string[]>
+}
+
+export interface PaginatedResponse<T> {
+  current_page: number;
+  data: T[];
+  first_page_url: string|null;
+  from: number;
+  last_page: number;
+  last_page_url: string | null;
+  links: {
+    url: string | null;
+    label: string;
+    active: boolean;
+  }[];
+  next_page_url: string | null;
+  path: string;
+  per_page: number;
+  prev_page_url: string | null;
+  to: number;
+  total: number;
+}
